b9d2c24f6203678ef2d0976e446c8f13e81ca312
Михайло Сеньків <mmsenkiv021@gmail.com>
2024-05-29 19:51:07 +0300

    Add project files.

---
+++ Composter.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Composter
+{
+    static private int lastId = 0;
+
+    private int id;
+    protected string code;
+    public List<Ticket> tickets = new List<Ticket>(1);
+    //public Ticket ticket;
+
+    public DateTime currentDateTime;
+    public DateTime startDateTime;
+    public DateTime lastDateTime;
+    public int processedTicketCount;
+    public List<int> processedTicketId = new List<int>();
+    public int workedHours;
+    private int Id
+    {
+        get { return id; }
+        set { if (value > 0) { id = value; } }
+    }
+    protected string Code
+    {
+        get { return code; }
+        set { code = value; }
+    }
+
+    static Composter()
+    {
+        lastId = 0;
+    }
+    public Composter()
+    {
+        id = MakeID();
+        processedTicketCount = 0;
+        workedHours = 0;
+        //tickets.Add(new Ticket());
+        code = "default";
+
+    }
+
+    public Composter(int Id, string Code, /*int ProcessedTicketCount,*/ int WorkedHours/*, int TicketCount*/) //redo
+    {
+        this.code = Code;
+        this.id = Id;
+        //this.processedTicketCount = ProcessedTicketCount;
+        this.workedHours = WorkedHours;
+    }
+
+    public static int MakeID()
+    {
+        return ++lastId;
+    }
+
+    public static string GetTicketType(Ticket ticket)
+    {
+        if (ticket.type == 1)
+        {
+            return "Single use ticket";
+        }
+        else if (ticket.type == 2)
+        {
+            return "Multi use ticket";
+        }
+        else
+        {
+            return "Invalid ticket type";
+        }
+    }
+    public void Check(Ticket ticket)
+    {
+        if (ticket.id > 10000 && ticket.id < 10100)
+        {
+            Console.WriteLine("Ticket is genuine.");
+        }
+        else
+        {
+            Console.WriteLine("Ticket is not genuine.");
+        }
+    }
+
+    public void Validate()
+    {
+        Console.WriteLine("Hours worked: " + workedHours);
+    }
+    public void Validate(Ticket ticket)
+    {
+        Check(ticket);
+
+        if (ticket.availableUses == 0)
+        {
+            Console.WriteLine("You have no available rides left.");
+        }
+        else if (ticket.availableUses >= 1)
+        {
+            ticket.availableUses--;
+            ticket.isValid = true;
+            processedTicketCount++;
+            processedTicketId.Add(ticket.id);
+            //ticket.code = this.code;
+            Console.WriteLine("Your " + GetTicketType(ticket) + "has been validated, uses left: " + ticket.availableUses);
+        }
+    }
+
+    public void Input()
+    {
+        Console.WriteLine("Input composter id: ");
+        int.TryParse(Console.ReadLine(), out id);
+        Console.WriteLine("Input code: ");
+        code = Console.ReadLine();
+        Console.WriteLine("Enter the number of tickets: ");
+        int.TryParse(Console.ReadLine(), out int size);
+        if (size > 0)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                tickets[i].Input();
+            }
+        }
+    }
+
+    public Composter LoadFromFile(string fileName)
+    {
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("Cannot find the file.");
+            return null;
+        }
+        try
+        {
+            string[] strings = File.ReadAllLines(fileName);
+
+            Composter newComposter = new Composter();
+            newComposter.id = int.Parse(strings[0]);
+            newComposter.code = strings[1];
+            newComposter.processedTicketCount = int.Parse(strings[2]);
+
+            //int i;
+            //for(i = 0; i < newComposter.processedTicketCount; i++)
+            //{
+            //    newComposter.processedTicketId.Add(Convert.ToInt32(strings[3+i]));
+            //}
+            newComposter.workedHours = int.Parse(strings[3]);
+
+            tickets[0].id = int.Parse(strings[4]);
+            //tickets[0].code = strings[5];
+            tickets[0].price = int.Parse(strings[5]);
+            tickets[0].type = int.Parse(strings[6]);
+            tickets[0].availableUses = int.Parse(strings[7]);
+            bool.TryParse(strings[8], out tickets[0].isValid);
+
+            return newComposter;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error reading from file: {e.Message}");
+            return null;
+        }
+
+
+    }
+
+    public void SaveToFile(string fileName)
+    {
+        try
+        {
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.WriteLine(id);
+                writer.WriteLine(code);
+                //writer.WriteLine(currentDateTime.ToShortDateString());
+                //writer.WriteLine(startDateTime);
+                //writer.WriteLine(lastDateTime);
+                writer.WriteLine(processedTicketCount);
+
+                //foreach(int num in processedTicketId)
+                //{
+                //    writer.WriteLine(num);
+                //}
+
+                writer.WriteLine(workedHours);
+
+                //foreach (Ticket ticket in tickets)
+                //{
+                writer.WriteLine(tickets[0].id);
+                //writer.WriteLine(tickets[0].code);
+                writer.WriteLine(tickets[0].price);
+                writer.WriteLine(tickets[0].type);
+                writer.WriteLine(tickets[0].availableUses);
+                writer.WriteLine(tickets[0].isValid.ToString());
+                //}
+
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error reading from file: {e.Message}");
+        }
+    }
+
+    public void BuyTicket()
+    {
+        Ticket newTicket = new Ticket();
+        newTicket.Buy();
+        tickets.Add(newTicket);
+    }
+
+    public override string ToString()
+    {
+        return "Id: " + id + ", code: " + code + ", processed ticket count: " + processedTicketCount + ", worked hours: " + workedHours;
+    }
+}
---
+++ Ticket.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Ticket  //make private fields, check constructors
+{
+    protected string code = "default";
+
+    public int id;
+    public int price; //{ get { return price; } set { if (value > 0) { price = value; } } }
+    public int type; //{ get { return type; } set { if (value == 1 || value == 2) { type = value; } } }        //1 - single use, 2 - multi use, maybe use bool
+    public int availableUses; //{ get { return availableUses; } set { if (value >= 0) { availableUses = value; } } }
+    public bool isValid;
+
+    protected string Code
+    {
+        get { return code; }
+        set { code = value; }
+    }
+    public Ticket()
+    {
+        Random rng = new Random();
+        price = 10;
+        type = 1;
+        isValid = false;
+        availableUses = 1;
+        id = 10000 + rng.Next(1, 100);
+    }
+
+    public Ticket(int type)
+    {
+
+        isValid = false;
+
+        if (type == 1)
+        {
+            availableUses = 1;
+            price = 10;
+        }
+        else if (type == 2)
+        {
+            availableUses = 5;
+            price = 50;
+
+        }
+        else
+        {
+            Console.WriteLine("Invalid ticket type."); //not sure if it is suitable 
+        }
+        this.type = type;
+        isValid = false;
+        Random rng = new Random();
+        id = 10000 + rng.Next(1, 100);
+    }
+    public Ticket(int price, int type, bool isValid, int availbleUses)
+    {
+
+        this.price = price;
+        this.type = type;
+        this.isValid = isValid;
+        this.availableUses = availbleUses;
+
+    }
+
+    public void Buy()
+    {
+        Console.WriteLine("Input type (1 - Single Use (10 Uah), 2 - Multiuse(5 uses, 50 uah): ");
+        type = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public void Input()
+    {
+        Console.WriteLine("Input id: ");
+        id = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input price: ");
+        price = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input type(1/2): ");
+        type = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input available uses: ");
+        availableUses = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input validation state");
+        isValid = Convert.ToBoolean(Console.ReadLine()); //?
+        Console.WriteLine("Input code: ");
+        code = Console.ReadLine();
+    }
+
+    public override string ToString()
+    {
+        return "Id: " + id + ", type: " + type + ", price: " + price + ", available uses: " + availableUses + ", is valid: " + isValid.ToString();
+    }
+}
